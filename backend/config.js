be5ab99b589a313732c66ff1a96aa8cc12222288
Challagundla Venkata sai teja <venkatasaitejachallagundla25@gmail.com>
2024-08-27 23:19:23 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://venkatasaitejachallagundla25:helloworlddb@books-store-mern.6oewz.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
+// This database will be deleted after tutorial
