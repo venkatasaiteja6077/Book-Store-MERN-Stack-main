f6bc2b21774c4d11ca2d8954a043b37ffca2fd98
venkatasaiteja6077 <124488925+venkatasaiteja6077@users.noreply.github.com>
2024-08-27 23:55:46 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+export const process.env.PORT || 5555;
 
 export const mongoDBURL =
   'mongodb+srv://venkatasaitejachallagundla25:helloworlddb@books-store-mern.6oewz.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
